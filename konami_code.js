40008b425df994fa9d1d94d4d78b575269435502
Shobhit Ratan <shobhit.ratan@baruchmail.cuny.edu>
2020-02-18 02:21:07 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -11,20 +11,24 @@ const codes = [
   "a"
 ];
 
-let index = 0;
 function init() {
   // your code here
-  document.body.addEventListener('keyword', (e) => {
-    const key = e.key;
-    if (key === codes[index]) {
-      index++;
-      if (index === codes.length) {
-        alert('Hurray!');
-        index = 0;
-      }
-    }
-    else {
-      index = 0;
-    }
-  })
+  let index = 0;
+
+document.body.addEventListener("keydown", (thingPressed) => {
+  const key = thingPressed.key
+  console.log(key)
+
+  if (codes[index] == key) {
+    ++index
+  }
+  else {
+    idx = 0
+  }
+  console.log(index)
+  if (index == 10) {
+    alert("Congratulations, you've entered the Konami Code!");
+    index = 0
+  }
+})
 }
